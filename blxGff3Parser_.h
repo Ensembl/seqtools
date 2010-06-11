666fd0a40e1fe66175a46938fc49f272cca60112
gb10 <gb10>
2010-06-11 09:38:31 +0000

    Added GFF3 parser

---
+++ blxGff3Parser_.h
@@ -0,0 +1,79 @@
+/*  File: blxGff3parser_.h
+ *  Author: Gemma Barson (gb10@sanger.ac.uk)
+ *  Copyright (c) 2010: Genome Research Ltd.
+ *-------------------------------------------------------------------
+ * Blixem is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * or see the on-line version at http://www.gnu.org/copyleft/gpl.txt
+ *-------------------------------------------------------------------
+ * Author: Gemma Barson (Sanger Institute, UK) gb10@sanger.ac.uk
+ *
+ * Description: parser for GFF version 3 format.
+ *              
+ * Exported functions: 
+ *-------------------------------------------------------------------
+ */
+
+#ifndef BLX_GFF_P_H
+#define BLX_GFF_P_H
+
+#include <gtk/gtk.h>
+#include <SeqTools/blixem_.h>
+
+/* Some defines for parsing stuff....may need v2 and v3 versions of these. */
+enum {GFF_MANDATORY_FIELDS = 8, GFF_MAX_FIELD_CHARS = 50, GFF_MAX_FREETEXT_CHARS = 5000} ;
+
+
+
+/* possible states for parsing GFF file, rather trivial in fact.... */
+typedef enum {BLXGFF_PARSE_HEADER, BLXGFF_PARSE_BODY,
+	      BLXGFF_PARSE_SEQUENCE, BLXGFF_PARSE_ERROR} BlxGFFParseState ;
+
+
+/* We follow glib convention in error domain naming:
+ *          "The error domain is called <NAMESPACE>_<MODULE>_ERROR" */
+#define BLX_GFF_ERROR "BLX_GFF_ERROR"
+
+typedef enum
+{
+  BLX_GFF_ERROR_HEADER,				    /* Error in GFF header section. */
+  BLX_GFF_ERROR_BODY,					    /* Error in GFF body section. */
+  BLX_GFF_ERROR_FAILED					    /* Other fatal failure, error->message
+							       should explain. */
+} BlxGFFError ;
+
+
+void parseGff3Header(const int lineNum,
+                     MSP **lastMsp, 
+		     MSP **mspList, 
+		     BlxParserState *parserState, 
+		     char *opts, 
+		     GString *line_string, 
+		     GList **seqList,
+                     char *refSeqName);
+
+void parseGff3Body(const int lineNum,
+                   MSP **lastMsp, 
+		   MSP **mspList, 
+		   BlxParserState *parserState, 
+		   char *opts, 
+		   GString *line_string, 
+		   GList **seqList);
+
+void parseFastaSeqHeader(char *line, const int lineNum,
+                         char **refSeq, char *refSeqName,
+                         char ***readSeq, int *readSeqLen, int *readSeqMaxLen,
+                         BlxParserState *parserState);
+
+#endif /* BLX_GFF_P_H */
