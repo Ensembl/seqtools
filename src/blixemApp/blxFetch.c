Ensembl/seqtools

commit a2b8b0087a9e2a78bbd2275b340db007e56da721

Column width settings are now saved on exit

src/blixemApp/blxFetch.c
@@ -824,7 +824,8 @@ gboolean populateFullDataPfetch(GList *seqsToFetch, const char *pfetchIP, int po
 }
 
 
-/* Set/Get global config, necessary because we don't have some blixem context pointer.... */
+/* Set/Get global config, necessary because we don't have some blixem context pointer....
+ * To do: we do have a context now, so this should be moved to there. */
 gboolean blxInitConfig(char *config_file, GError **error)
 {
   gboolean result = FALSE ;
@@ -833,19 +834,64 @@ gboolean blxInitConfig(char *config_file, GError **error)
 
   blx_config_G = g_key_file_new() ;
 
+  /* First, get any persistent settings from the settings file */
+  char *settings_file = blxprintf("%s/%s", g_get_home_dir(), BLIXEM_SETTINGS_FILE);
+  gchar *content1 = NULL;
+  gsize len1 = 0;
+
+  if (g_file_test(settings_file, G_FILE_TEST_EXISTS))
+    {
+      /* This will become the global config file if no other file is given. If
+       * another is given, this will be overwritten, so save the content of the settings file. */
+      g_key_file_load_from_file(blx_config_G, settings_file, G_KEY_FILE_NONE, NULL);
+      content1 = g_key_file_to_data(blx_config_G, &len1, NULL);
+    }
+  
+  /* Load the given config file, if any */
   if (!config_file)
     {
       result = TRUE ;
     }
+  else if (readConfigFile(blx_config_G, config_file, error))
+    {
+      result = TRUE;
+
+      if (content1)
+        {
+          /* Merge both file contents. First, get the new content as a string and concatenate */
+          gsize len2 = 0;
+          gchar *content2 = g_key_file_to_data(blx_config_G, &len2, NULL);
+
+          gsize len = len1 + len2 + 1;
+          gchar *content = g_malloc(len);
+          sprintf(content, "%s\n%s", content1, content2);
+
+          /* Load the concatenated contents into a new key file */
+          GKeyFile *key_file = g_key_file_new();
+          if (g_key_file_load_from_data(key_file, content, len, G_KEY_FILE_NONE, NULL))
+            {
+              /* Delete the original config key file and replace it with the new one */
+              g_key_file_free(blx_config_G);
+              blx_config_G = key_file;
+            }
+          else
+            {
+              g_key_file_free(key_file);
+            }
+
+          g_free(content);
+          g_free(content2);
+        }
+    }
   else
     {
-      if (!(result = readConfigFile(blx_config_G, config_file, error)))
-	{
-	  g_key_file_free(blx_config_G) ;
-	  blx_config_G = NULL ;
-	}
+      g_key_file_free(blx_config_G) ;
+      blx_config_G = NULL ;
     }
 
+  if (content1)
+    g_free(content1);
+  
   return result ;
 }
 

src/blixemApp/blixem_.h
@@ -110,6 +110,10 @@ AUTHOR_TEXT "\n"
 #define BLX_FETCH_REGION           REGION_FETCH_GROUP
 
 
+/* For settings */
+#define BLIXEM_SETTINGS_FILE             ".blixemrc"  /* default file name for saving blixem settings to */
+
+
 /* would be good to get rid of this.... */
 #define FULLNAMESIZE               255
 

src/blixemApp/blxwindow.c
@@ -4642,10 +4642,37 @@ static void destroyBlxContext(BlxViewContext **bc)
 }
 
 
+/* This function saves all of blixem's customisable settings to
+ * a config file. */
+static void saveBlixemSettings(GtkWidget *blxWindow)
+{
+  BlxViewContext *bc = blxWindowGetContext(blxWindow);
+  
+  char *filename = blxprintf("%s/%s", g_get_home_dir(), BLIXEM_SETTINGS_FILE);
+
+  GKeyFile *key_file = g_key_file_new();
+  GKeyFileFlags flags = G_KEY_FILE_NONE;
+  GError *error = NULL;
+  
+  /* Load existing contents so they can be merged, if the file already exists */
+  g_key_file_load_from_file(key_file, filename, flags, &error);
+  
+  g_message("Saving Blixem settings to '%s'.\n", filename);
+  detailViewSaveColumnWidths(blxWindowGetDetailView(blxWindow), key_file);
+
+  gchar *file_content = g_key_file_to_data(key_file, NULL, NULL);
+      
+  if (!g_file_set_contents(filename, file_content, -1, NULL))
+    g_warning("Error saving settings to '%s'.\n", filename);
+}
+
+
 static void onDestroyBlxWindow(GtkWidget *widget)
 {
   BlxWindowProperties *properties = blxWindowGetProperties(widget);
   
+  saveBlixemSettings(widget);
+
   if (properties)
     {
       destroyBlxContext(&properties->blxContext);

src/blixemApp/detailview.c
@@ -62,7 +62,6 @@
 #define POLYA_SIG_BASES_UPSTREAM        50    /* the number of bases upstream from a polyA tail to search for polyA signals */
 
 /* Define the columns' default widths and titles. */
-#define COLUMN_WIDTHS_GROUP             "column-widths"  /* group name in the config file */
 #define BLXCOL_INT_COLUMN_WIDTH		40    /* default width for ordinary integer columns */
 #define BLXCOL_SEQNAME_WIDTH            120   /* default width for the name column */
 #define BLXCOL_ID_WIDTH                 45    /* default width for the ID column */
@@ -76,6 +75,7 @@
 #define BLXCOL_STRAIN_WIDTH             85    /* default width for strain column  */
 #define BLXCOL_TISSUE_TYPE_WIDTH        100   /* default width for tissue-type column  */
 
+#define COLUMN_WIDTHS_GROUP             "column-widths"  /* group name in the config file */
 
 
 
@@ -159,6 +159,33 @@ const char* detailViewGetColumnTitle(GtkWidget *detailView, const BlxColumnId co
     }
   
   return result;
+}
+
+
+/* Save the column widths to the given config file. */
+void detailViewSaveColumnWidths(GtkWidget *detailView, GKeyFile *key_file)
+{
+  /* Loop through each column */
+  DetailViewProperties *properties = detailViewGetProperties(detailView);
+  GList *listItem = properties->columnList;
+  
+  for ( ; listItem; listItem = listItem->next)
+    {
+      DetailViewColumnInfo *columnInfo = (DetailViewColumnInfo*)(listItem->data);
+
+      if (columnInfo)
+        {
+          /* Don't save the width for the sequence column because it is dynamic */
+          if (columnInfo->columnId == BLXCOL_SEQUENCE)
+            continue;
+
+          /* If column is not visible, set width to zero to indicate that it should be hidden */
+          if (columnInfo->visible)
+            g_key_file_set_integer(key_file, COLUMN_WIDTHS_GROUP, columnInfo->title, columnInfo->width);
+          else
+            g_key_file_set_integer(key_file, COLUMN_WIDTHS_GROUP, columnInfo->title, 0);
+        }
+    }
 }
 
 

src/blixemApp/detailview.h
@@ -175,6 +175,7 @@ int			detailViewGetColumnWidth(GtkWidget *detailView, const BlxColumnId columnId
 const char*             detailViewGetColumnTitle(GtkWidget *detailView, const BlxColumnId columnId);
 void                    detailViewGetColumnXCoords(DetailViewProperties *properties, const BlxColumnId columnId, IntRange *xRange);
 gboolean                detailViewShowColumn(DetailViewColumnInfo *columnInfo);
+void                    detailViewSaveColumnWidths(GtkWidget *detailView, GKeyFile *key_file);
 
 int			getBaseIndexAtColCoords(const int x, const int y, const gdouble charWidth, const IntRange const *displayRange);
 

commit 3f99f53f17bc0754331694767dcc15dd31001fcc

Fixed a buffer overrun bug introduced in commit a2b8b00

src/blixemApp/blxFetch.c
@@ -858,7 +858,7 @@ gboolean blxInitConfig(char *config_file, GError **error)
           gsize len2 = 0;
           gchar *content2 = g_key_file_to_data(blx_config_G, &len2, NULL);
 
-          gsize len = len1 + len2 + 1;
+          gsize len = len1 + len2 + 2; /* need 2 extra chars for the sprintf below; newline and terminating nul */
           gchar *content = g_malloc(len);
           sprintf(content, "%s\n%s", content1, content2);
 