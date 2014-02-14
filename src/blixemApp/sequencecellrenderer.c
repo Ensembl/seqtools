e33ff0df8a516ce4e29b513354cd886428c73d31
Gemma Barson <gb10@sanger.ac.uk>
2014-02-14 15:06:38 +0000

    Fix problem with abstract type flag

--- src/blixemApp/sequencecellrenderer.c
+++ src/blixemApp/sequencecellrenderer.c
@@ -296,7 +296,7 @@ sequence_cell_renderer_get_type (void)
       cell_sequence_type = g_type_register_static (GTK_TYPE_CELL_RENDERER,
 						   "SequenceCellRenderer",
 						   &cell_sequence_info,
-                                                   G_TYPE_FLAG_VALUE_ABSTRACT);
+                                                   0);
     }
   
   return cell_sequence_type;
