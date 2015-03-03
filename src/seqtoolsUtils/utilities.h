7cf6861df0e912d285ae633f0b5637403b799fac
Gemma Barson <gb10@sanger.ac.uk>
2015-03-03 12:33:18 +0000

    "Remove debug output - TRIVIAL"

--- src/seqtoolsUtils/utilities.h
+++ src/seqtoolsUtils/utilities.h
@@ -66,7 +66,6 @@
 
 
 /* Debug logging macros. #define DEBUG to enable debug output. */
-#define DEBUG
 
 #ifdef DEBUG
 #define DEBUG_OUT(format, args...) debugLogLevel(0); printf(format, ##args); fflush(stdout);
