bc8394c254e3b1dd7f422cce959c2fd554528f25
Ed Griffiths <edgrif@sanger.ac.uk>
2016-01-18 11:08:30 +0000

    Fix up gbtools stuff. - BUILD

--- src/autogen.sh
+++ src/autogen.sh
@@ -19,6 +19,69 @@ else
    BASE_DIR=$SCRIPT_DIR
 fi
 
+
+# Cleans the sub-directory out and refetches the empty placeholder
+# from the zmap repository.
+#
+# Args are:    lib_repository_name  lib_local_dir
+#
+function clean_lib
+{
+    echo "Starting removing $1 from $2"
+
+    rm -rf ./$2/*
+
+    git checkout ./$2
+
+    echo "Finished removing $1 from $2"
+
+}
+
+
+# Sets up a sub-directory to contain the supplied git repository code.
+#
+# Args are:    full_git_repository_uri lib_local_dir git_branch
+#
+#
+function fetch_lib
+{
+    tmp_dir='autogen_tmp_git_checkout_dir'
+
+    echo "Starting cloning $1 into $2"
+
+    mkdir $tmp_dir
+
+    git clone $3 $1 $tmp_dir || zmap_message_exit "could not clone $1 into $PWD/$tmp_dir"
+
+    cp -rf ./$tmp_dir/* ./$2
+
+    rm -rf ./$tmp_dir
+
+    # SHOULD WE BE DOING THIS ????? NOT TOO SURE....
+    # Make sure the placeholder files (.gitignore, README) are their original zmap versions
+    #git checkout ./$2/
+
+    echo "Finished cloning $1 into $2"
+
+}
+
+
+
+# Should gbtools be installed ? Default is "no"
+gbtools_install=''
+
+
+
+while getopts ":g" opt ; do
+    case $opt in
+	g  ) gbtools_install='yes' ;;
+    esac
+done
+
+
+
+
+
 # set up seqtools version number, this is arcane and horrible and all
 # autoconf's fault. See http://sources.redhat.com/automake/automake.html#Rebuilding
 # and the stuff about AC_INIT
@@ -34,11 +97,21 @@ echo "m4_define([VERSION_NUMBER], [$SEQTOOLS_VERSION])" >> $version_macro_file
 echo "SeqTools version is: $SEQTOOLS_VERSION"
 
 
+# Should gbtools be installed ?
+if [ -n gbtools_install ] ; then
+
+  clean_lib 'gbtools' ./gbtools
+
+  fetch_lib git.internal.sanger.ac.uk:/repos/git/annotools/gbtools  'gbtools'
+
+fi
+
+
 # If the gbtools autogen.sh script exists then run that. This is necessary
 # for gbtools to create its gbtools_version.m4 file.
 #
 if [ -e "$BASE_DIR/gbtools/autogen.sh" ] ; then
-  echo "found"
+  echo "found gbtools autogen.sh so running it"
   cur_dir=`pwd`
   cd $BASE_DIR/gbtools
   ./autogen.sh
@@ -47,4 +120,7 @@ fi
 
 
 # then for now we just run autoconf
-autoreconf -fi
+autoreconf -fi -v || echo "autoreconf failed...."
+
+
+
