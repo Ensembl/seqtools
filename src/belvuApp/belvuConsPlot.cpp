9f41ea001795537f275e0676b4c8902dd3548113
Tiago Grego <tgrego@ebi.ac.uk>
2020-01-09 15:12:22 +0000

    Yearly copyright update

--- src/belvuApp/belvuConsPlot.cpp
+++ src/belvuApp/belvuConsPlot.cpp
@@ -1,6 +1,6 @@
 /*  File: belvuConsPlot.h
  *  Author: Gemma Barson, 2011-07-01
- *  Copyright [2018-2019] EMBL-European Bioinformatics Institute
+ *  Copyright [2018-2020] EMBL-European Bioinformatics Institute
  *  Copyright (c) 2006-2017 Genome Research Ltd
  * ---------------------------------------------------------------------------
  * Licensed under the Apache License, Version 2.0 (the "License");
--- src/blixemApp/bigpicture.hpp
+++ src/blixemApp/bigpicture.hpp
@@ -1,6 +1,6 @@
 /*  File: bigpicture.h
  *  Author: Gemma Barson, 2009-11-23
- *  Copyright [2018-2019] EMBL-European Bioinformatics Institute
+ *  Copyright [2018-2020] EMBL-European Bioinformatics Institute
  *  Copyright (c) 2006-2017 Genome Research Ltd
  * ---------------------------------------------------------------------------
  * Licensed under the Apache License, Version 2.0 (the "License");
